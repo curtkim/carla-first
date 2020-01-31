f33bf8b12980e0dd60ce66d538ed326247a247f3
curt.k <curt.k@kakaocorp.com>
2020-01-31 17:03:52 +0900

    map-first수정

--- map-first.py
+++ map-first.py
@@ -11,11 +11,12 @@ def main():
         print(map.transform_to_geolocation(carla.Location(0,0,0)))
         wp1 = map.get_waypoint(carla.Location(0,0,0))
         print(wp1)
-        print(wp1.next(10)[0])
+        print(f"road_id: {wp1.road_id}, section_id: {wp1.section_id}, lane_id: {wp1.lane_id}, is_junction: {wp1.is_junction}")
+        print(wp1.next(1)[0])
 
-        topo = map.get_topology()
-        for (a, b) in topo:
-            print(a.transform.location.distance(b.transform.location), a, b)
+        # topo = map.get_topology()
+        # for (a, b) in topo:
+        #     print(a.transform.location.distance(b.transform.location), a, b)
 
 if __name__ == '__main__':
     main()
